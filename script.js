82db45626e3c17decc7a6db8ed714c1cedd78d7f
Inha Kim <dlsgk0753@gmail.com>
2026-08-10 13:23:43 +0900

    almost final

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//alert("Hello, World!");
+const modal = document.querySelector('#modal');
+document.querySelector('.button').addEventListener('click', () =>
+    modal.classList.add('is-open'));
+document.querySelector('#closeBtn').addEventListener('click', () =>
+    modal.classList.remove('is-open'));
+modal.addEventListener('click', (e) => {
+    if (e.target.id === 'modal') {
+        modal.classList.remove('is-open');
+    }
+});
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modal.classList.remove('is-open');
+    }
+});
